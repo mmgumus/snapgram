d514fdf71055915fa689549c252665e5c4b20dfb
mmgumus <mahmut_muhammet@hotmail.com>
2023-12-15 16:45:40 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './global.css';
+
+function App() {
+   // JSX içeriği döndür
+   return (
+      <div className="text-3xl font-bold bg-blue-300 underline">
+         Snapgram App
+      </div>
+   );
+}
+
+export default App;
